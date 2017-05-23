c1198e1b3758eac259f02e0946ef58397a276d13
VinothiniVijayan <vinothiniviji1@gmail.com>
2017-05-23 18:58:20 +0530

    Add files via upload

---
+++ page_flip/page.js
@@ -0,0 +1,32 @@
+console.log(newsDetailObject);
+var	bb, $container;
+
+function bookblock() {
+	$container = $( '#container' );
+	bb = $( '#bb-bookblock' ).bookblock( {
+		speed : 800,
+		perspective : 2000,
+		shadowSides	: 0.8,
+		shadowFlip	: 0.4,
+		onEndFlip : function(old, page, isLimit) {
+			current = page;
+		}
+	} ),
+	$navNext = $( '#bb-nav-next' ),
+	$navPrev = $( '#bb-nav-prev' ).hide(),
+	$menuItems = $container.find( 'ul.menu-toc > li' ),
+	$tblcontents = $( '#tblcontents' ),
+	transEndEventNames = {
+		'WebkitTransition': 'webkitTransitionEnd',
+		'MozTransition': 'transitionend',
+		'OTransition': 'oTransitionEnd',
+		'msTransition': 'MSTransitionEnd',
+		'transition': 'transitionend'
+	},
+	transEndEventName = transEndEventNames[Modernizr.prefixed('transition')],
+	supportTransitions = Modernizr.csstransitions;
+}
+
+DOMCheck('.bb-item:nth-child('+newsDetailObject+')', bookblock);
+DOMCheck('.bb-item:nth-child('+newsDetailObject+')', swipeLeft);
+DOMCheck('.bb-item:nth-child('+newsDetailObject+')', swipeRight);
