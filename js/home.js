a6f37e7b4f9a3fca22b49553750d868de64d3fb3
VinothiniVijayan <vinothiniviji1@gmail.com>
2017-05-23 23:37:38 +0530

    Add files via upload

--- js/home.js
+++ js/home.js
@@ -1,3 +1,7 @@
 app.controller("homeCtrl",['$scope',function($scope){
     console.log("Home"); 
+	var list = [{'image':'./img/image1.jpg','content':'Image 1'},
+		{'image':'./img/image2.jpg','content':'Image 1'},
+			{'image':'./img/image3.png','content':'Image 1'}]
+	$scope.List = angular.copy(list);
 }])
