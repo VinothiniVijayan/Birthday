90e7c69cdcb5a2ebb6a3b765bac4abad7ee944e6
VinothiniVijayan <vinothiniviji1@gmail.com>
2017-05-24 18:58:32 +0530

    Add files via upload

--- js/home.js
+++ js/home.js
@@ -1,3 +1,35 @@
 app.controller("homeCtrl",['$scope',function($scope){
     console.log("Home"); 
+	var list = [{'image':'./img/image1.jpg','content':'Image 1'},
+		{'image':'./img/image2.jpg','content':'Image 1'},
+			{'image':'./img/image3.png','content':'Image 1'}];
+	$scope.List = angular.copy(list);
+	$scope.flipCount = 0;
+    $scope.totalCount = $scope.List.length;
+	swipeLeft = function () {
+ 		$('.bb-item').on('swipeleft', function(){
+ 			bb.next();
+			if( $scope.flipCount < $scope.totalCount - 1 ) {
+		    	$scope.flipCount += 1;
+		    }
+			console.log($scope.flipCount);
+			/*if( $scope.flipCount < 2 ) {
+				$scope.flipCount += 1;
+			}*/
+			return false;
+ 		});
+    };
+    swipeRight = function () {
+    	$('.bb-item').on('swiperight', function(){
+    		bb.prev();
+		    if( $scope.flipCount >  0) {
+		    	$scope.flipCount -= 1;
+		    }
+			console.log($scope.flipCount);
+			/*if( $scope.flipCount > 1 ) {
+				$scope.flipCount -= 1;
+			}*/
+			return false;
+    	});
+    };
 }])
