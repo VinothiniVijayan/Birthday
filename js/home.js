e1a3f5f12591f5343f24cfd35a36cf2033680c7b
VinothiniVijayan <vinothiniviji1@gmail.com>
2017-05-25 11:39:34 +0530

    Add files via upload

--- js/home.js
+++ js/home.js
@@ -1,3 +1,4 @@
+var listCount;
 app.controller("homeCtrl",['$scope',function($scope){
     console.log("Home"); 
 	var list = [{'image':'./img/image1.jpg','content':'Image 1'},
@@ -6,6 +7,7 @@ app.controller("homeCtrl",['$scope',function($scope){
 	$scope.List = angular.copy(list);
 	$scope.flipCount = 0;
     $scope.totalCount = $scope.List.length;
+	listCount = $scope.List.length;
 	swipeLeft = function () {
  		$('.bb-item').on('swipeleft', function(){
  			bb.next();
@@ -32,4 +34,28 @@ app.controller("homeCtrl",['$scope',function($scope){
 			return false;
     	});
     };
+	DOMCheck = function(DOM, callbackFunc) {
+    	console.log('start DOMCheck');
+        try {
+            if (DOM) {
+                var intFlag;
+                if ($(DOM).length <= 0) {
+                    intFlag = setInterval(function() {
+                        setTimeout(function() {
+                            clearInterval(intFlag);
+                        }, 10000);
+                        if ($(DOM).length > 0) {
+                            clearInterval(intFlag);
+                            callbackFunc();
+                        }
+                    }, 100);
+                } else {
+                    callbackFunc();
+                }
+                console.log('DOM check last element loaded');
+            }
+        } catch (e) {
+            console.log('Error in DOMCheck function' + e);
+        }
+    }
 }])
